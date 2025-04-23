1bf29816ffd7ac081efc20033ef9cdefc795e055
fahadnaveed159 <fahadnaveed159@gmail.com>
2025-04-23 20:53:37 +0500

    setup project

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log(navbar)
+
+function openMenu(){
+const navbar = document.getElementById("navb")
+const list = document.getElementById("list")
+    navbar.classList.toggle("navbar")
+    list.classList.toggle("mobileList")
+    
+}
